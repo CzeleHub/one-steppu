ba999cf82c71538f4e1509308e5e680a557b5f1b
CzeleHub <szymondyktyserviam@gmail.com>
2026-03-06 14:06:26 +0100

    fix nvidia webkit_dmabuf issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // see https://forums.developer.nvidia.com/t/webkit-tauri-application-white-screen-on-dgx-spark-gpupermission-issues/348741/2
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     onesteppu_lib::run()
 }
